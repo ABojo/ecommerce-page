e26b6e40f8083557899ab78ecf4eab6c218a6fbc
Alex <alex@ALEX-LAPTOP.local>
2023-07-14 18:38:44 -0400

    wrapped the app in the cart provider and passed in all of the necessary props to the product component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import Header from "./components/Header/Header";
 import Product from "./components/Product/Product";
+import { CartProvider } from "./contexts/CartContext";
+import buildImagePaths from "./utils/buildImagePaths";
+
+const { images, thumbnails } = buildImagePaths();
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Product
-        brand={"Sneaker Company"}
-        name={"Fall Limited Edition Sneakers"}
-        description={
-          "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer."
-        }
-        price={250}
-        discount={50}
-        images={[""]}
-      />
-    </div>
+    <CartProvider>
+      <div className="container">
+        <Header />
+        <Product
+          brand={"Sneaker Company"}
+          name={"Fall Limited Edition Sneakers"}
+          description={
+            "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer."
+          }
+          price={250}
+          discount={50}
+          images={images}
+          thumbnails={thumbnails}
+        />
+      </div>
+    </CartProvider>
   );
 }
 
