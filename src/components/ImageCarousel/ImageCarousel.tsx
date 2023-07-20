86f7a69d7d5278c9e6cb8063b583b50aa1e761aa
Alex <alex@ALEX-LAPTOP.local>
2023-07-20 14:27:26 -0400

    Made carousel responsive. Added arrows controls that will be shown on smaller screens. Added a media query to hide the lightbox trigger button once the arrow controls are visible. Pulled in the useCounter hook for the index state.

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Lightbox from "../Lightbox/Lightbox";
 import styles from "./ImageCarousel.module.scss";
+import useCounter from "../../hooks/useCounter";
 
 interface ImageCarouselProps {
   images: string[];
@@ -8,14 +9,14 @@ interface ImageCarouselProps {
 }
 
 export default function ImageCarousel({ images, thumbnails }: ImageCarouselProps) {
-  const [imageIndex, setImageIndex] = useState(0);
+  const { counter, decrementCounter, incrementCounter, setCounter } = useCounter(images.length - 1);
   const [showLightbox, setShowLightbox] = useState(false);
 
   return (
     <div className={styles.carousel}>
       {showLightbox ? (
         <Lightbox
-          startIndex={imageIndex}
+          startIndex={counter}
           images={images}
           thumbnails={thumbnails}
           onClose={() => {
@@ -23,21 +24,32 @@ export default function ImageCarousel({ images, thumbnails }: ImageCarouselProps
           }}
         />
       ) : null}
-      <button className={styles.carousel__main} onClick={() => setShowLightbox(true)}>
-        <img src={images[imageIndex]} alt="Main" />
-      </button>
+      <div className={styles.carousel__main}>
+        <button className={styles.carousel__lightbox} onClick={() => setShowLightbox(true)}></button>
+        <button className={styles.carousel__left} onClick={decrementCounter}>
+          <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 1 3 9l8 8" stroke="currentColor" stroke-width="3" fill="none" />
+          </svg>{" "}
+        </button>
+        <button className={styles.carousel__right} onClick={incrementCounter}>
+          <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="m2 1 8 8-8 8" stroke="currentColor" stroke-width="3" fill="none" />
+          </svg>{" "}
+        </button>
+        <img src={images[counter]} alt="Main" />
+      </div>
       <div className={styles.carousel__thumbnails}>
         {thumbnails.map((thumbnail, i) => {
           let buttonClass = styles.carousel__thumbnail;
 
-          if (i === imageIndex) buttonClass += ` ${styles["carousel__thumbnail--active"]}`;
+          if (i === counter) buttonClass += ` ${styles["carousel__thumbnail--active"]}`;
 
           return (
             <button
               key={i}
               className={buttonClass}
               onClick={() => {
-                setImageIndex(i);
+                setCounter(i);
               }}
             >
               <img src={thumbnail} alt="Thumbnail" />
