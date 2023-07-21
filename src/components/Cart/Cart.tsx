45bcd4833bb6c6f6a6f3fa7cf41079d3f8b53619
Alex <alex@ALEX-LAPTOP.local>
2023-07-21 14:13:39 -0400

    made cart responsive on smaller screens and added a close button to the head of the dropdown so its easy to close when scrolling on smaller screens

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -25,7 +25,18 @@ export default function Cart() {
       </button>
       {isOpen ? (
         <div className={styles.dropdown}>
-          <div className={styles.dropdown__head}>Cart</div>
+          <div className={styles.dropdown__head}>
+            <span>Cart</span>
+            <button className={styles.dropdown__close} onClick={toggleCart}>
+              <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
           <div className={styles.dropdown__body}>
             {cartItems.length ? (
               <>
